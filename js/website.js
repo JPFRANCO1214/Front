9ba4715bd997da79d58e7bc2773cfe155929951a
Juan Pablo1 <juanpablofrancoortiz@gmail.com>
2024-05-03 19:07:03 -0600

    CAMBIOS FRONT

--- js/website.js
+++ js/website.js
@@ -4,3 +4,16 @@ function startJourney() {
 function redirigir() {
     setTimeout(function() {window.location.href = "loadjourney.html?redirect=true";}, 250);
 }
+
+  var qrVisible = false;
+
+  function showQR() {
+    var qrContainer = document.getElementById("qr-container");
+    if (qrVisible) {
+      qrContainer.style.display = "none";
+      qrVisible = false;
+    } else {
+      qrContainer.style.display = "block";
+      qrVisible = true;
+    }
+  }
