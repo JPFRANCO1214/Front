1e76ad395100cebb3fa44c525bf460138d131e84
Juan Pablo1 <juanpablofrancoortiz@gmail.com>
2024-05-06 07:25:15 -0600

    QR

--- js/journey.js
+++ js/journey.js
@@ -1,3 +1,16 @@
 function redirigir() {
     setTimeout(function() {window.location.href = "loadwebsite.html?redirect=true";}, 250);
 }
+
+var qrVisible = false;
+
+function showQR() {
+  var qrContainer = document.getElementById("qr-container");
+  if (qrVisible) {
+    qrContainer.style.display = "none";
+    qrVisible = false;
+  } else {
+    qrContainer.style.display = "block";
+    qrVisible = true;
+  }
+}
